6978e400146edac8ac82806a7fa6e682cd95b3f1
Matthew Cania <matthewgab24@gmail.com>
2025-12-27 20:58:34 +0800

    Frontend Session Based Header Done (No Actual Interaction with Backend)

--- Frontend/src/components/partials/header.jsx
+++ Frontend/src/components/partials/header.jsx
@@ -9,37 +9,107 @@ import {
   NavigationMenuTrigger,
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { useState } from "react";
 
 export default function Header() {
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(false) //Temporary
+
+  // TODO - Fetch user session
+  // if (!user) {
+  //   setLoading(true)
+  //   setLoading(false)
+  // }
+
   return (
-    <header className='flex w-full justify-between items-center h-12 bg-orange-500 text-white px-2'>
-      <Link to={"/"} className="text-center h-auto">
-      <img
-        src="/NutriBin_Logo.svg"
-        alt="NutriBin Logo"
-        className="h-8 px-4"
-      />
+    <>
+      {loading ? (
+        <header className='flex w-full justify-between items-center h-12 bg-orange-500 text-white px-2'>
+          <h1>Loading...</h1>
+        </header>
+      ) :
+        (
+
+          <header className='flex w-full justify-between items-center h-12 bg-orange-500 text-white px-2'>
+            <Link to={"/"} className="text-center h-auto">
+              <img
+                src="/NutriBin_Logo.svg"
+                alt="NutriBin Logo"
+                className="h-8 px-4"
+              />
 
 
-      </Link>
-      <nav className="flex w-auto px-2 gap-4">
-        <Button asChild className={"bg-transparent hover:bg-amber-700"}>
-          <Link to={'/login'}>
-            Home
-          </Link>
-          </Button>
-        <Button asChild className={"bg-transparent hover:bg-amber-700"}>
-          <Link to={'/guide'}>
-            Guide
-          </Link>
-          </Button>
-        <Button asChild className={"bg-transparent hover:bg-amber-700"}>
-          <Link to={'/register'}>
-            Register
-          </Link>
-          </Button>
-      </nav>
-    </header>
+            </Link>
+            {user ? (
+              <nav className="flex gap-2">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button className={'bg-transparent border-none outline-none'} variant="outline">MattCania
+                      <Avatar className={'m-1'}>
+                        {/* Arbitrary Image */}
+                        <AvatarImage src="https://github.com/MattCania.png" />
+                        <AvatarFallback>CN</AvatarFallback>
+                      </Avatar>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-56" align="start">
+                    <DropdownMenuLabel>matthewgab24@gmail.com</DropdownMenuLabel>
+                    <DropdownMenuGroup>
+                      <DropdownMenuItem>
+                        Profile
+                        {/* <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut> */}
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>
+                        Settings
+                        {/* <DropdownMenuShortcut>⌘S</DropdownMenuShortcut> */}
+                      </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                      Log out
+                      {/* <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut> */}
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </nav>
+            ) : (
+
+              <nav className="flex w-auto px-2 gap-4">
+                <Button asChild className={"bg-transparent hover:bg-amber-700"}>
+                  <Link to={'/login'}>
+                    Home
+                  </Link>
+                </Button>
+                <Button asChild className={"bg-transparent hover:bg-amber-700"}>
+                  <Link to={'/guide'}>
+                    Guide
+                  </Link>
+                </Button>
+                <Button asChild className={"bg-transparent hover:bg-amber-700"}>
+                  <Link to={'/register'}>
+                    Register
+                  </Link>
+                </Button>
+              </nav>
+            )}
+          </header>
+        )}
+    </>
   );
 }
--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -98,7 +98,7 @@ export function Login() {
 						</div>
 
 						<Button type='button' className="bg-secondary hover:bg-secondary-foreground w-full cursor-pointer">
-							<svg class="mr-2 h-5 w-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							<svg className="mr-2 h-5 w-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 								<path
 									fill="currentColor"
 									d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
