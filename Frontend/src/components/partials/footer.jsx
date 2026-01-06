5a47fe2b914cfd210e9e2d08a90ebfee9a48328f
Yurei <69757457+Yurei21@users.noreply.github.com>
2026-01-06 20:49:48 +0800

    bruh

--- Frontend/src/components/partials/footer.jsx
+++ Frontend/src/components/partials/footer.jsx
@@ -13,36 +13,26 @@ import { Button } from "../ui/button";
 
 export default function Footer() {
   return (
-    <header className='flex w-full justify-between items-center h-15 bg-orange-500 text-white'>
+    <header className="flex w-full justify-between items-center h-15 bg-orange-500 text-white">
       <nav className="flex w-auto px-2">
         <div>
           <Button asChild className={"bg-transparent hover:bg-amber-700"}>
-            <Link to={'/about'}>
-              About Us
-            </Link>
+            <Link to={"/about"}>About Us</Link>
           </Button>
           <Button asChild className={"bg-transparent hover:bg-amber-700"}>
-            <Link to={'/faqs'}>
-              FAQs
-            </Link>
+            <Link to={"/faqs"}>FAQs</Link>
           </Button>
           <Button asChild className={"bg-transparent hover:bg-amber-700"}>
-            <Link to={'/policies'}>
-              Terms of Service
-            </Link>
-          </Button>
-        </div>
-        <div>
-
-          <Button asChild className={"bg-transparent hover:bg-amber-700"}>
-            <Link to={'/socials'}>
-              Socials
-            </Link>
+            <Link to={"/policies"}>Terms of Service</Link>
           </Button>
           <Button asChild className={"bg-transparent hover:bg-amber-700"}>
-            <Link to={'/studies'}>
-              Studies
-            </Link>
+            <Link to={"/socials"}>Socials</Link>
+          </Button>
+          <Button asChild className={"bg-transparent hover:bg-amber-700"}>
+            <Link to={"/studies"}>Studies</Link>
+          </Button>
+          <Button asChild className={"bg-transparent hover:bg-amber-700"}>
+            <Link to={"/guide"}>Guide</Link>
           </Button>
         </div>
       </nav>
